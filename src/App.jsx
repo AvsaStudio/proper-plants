e8b893245acf188a29fe33c737143da58094f4a6
AvsaStudio <salome.a.miller@gmail.com>
2026-05-16 14:22:18 -0400

    finished with project

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,51 @@
+import { useState } from "react";
+import PLANTS from "./data";
+
+import Cart from "./cart/Cart";
+import Plants from "./plants/plants";
+
 export default function App() {
-  return <></>;
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (plant) => {
+    const itemExists = cart.find((item) => item.id === plant.id);
+
+    if (itemExists) {
+      setCart(
+        cart.map((item) =>
+          item.id === plant.id ? { ...item, quantity: item.quantity + 1 } : item
+        )
+      );
+    } else {
+      setCart([...cart, { ...plant, quantity: 1 }]);
+    }
+  };
+
+  const removeFromCart = (itemToRemove) => {
+    setCart(
+      cart
+        .map((item) =>
+          item.id === itemToRemove.id
+            ? { ...item, quantity: item.quantity - 1 }
+            : item
+        )
+        .filter((item) => item.quantity > 0)
+    );
+  };
+
+  return (
+    <>
+      <h1>Proper Plants</h1>
+
+      <main>
+        <Plants plants={PLANTS} addToCart={addToCart} />
+
+        <Cart
+          cart={cart}
+          removeFromCart={removeFromCart}
+          addToCart={addToCart}
+        />
+      </main>
+    </>
+  );
 }
